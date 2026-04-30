36a5deb0d79608742f5a27f814a0c94a0317bc3e
whan789 <102964433+whan789@users.noreply.github.com>
2026-04-30 11:59:35 +0900

    Title: 함수를 이용한 윤년 판별, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260430/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,14 @@
+y = int(input())
+
+# Please write your code here.
+def sol(year):
+    if year % 4 ==0:
+        return True
+    if (year % 100 == 0) and (year % 400 !=0):
+        return True
+    return False
+
+if sol(y):
+    print('true')
+else:
+    print('false')
