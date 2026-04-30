a0be6044ce730fa2ca4f573d3e74632e4773559e
whan789 <102964433+whan789@users.noreply.github.com>
2026-04-30 11:53:08 +0900

    Title: 함수를 이용한 369 게임, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260430/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,20 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+def sol1(N):
+    return any(char in str(N) for char in '369')
+    
+
+def sol2(N):
+    return N % 3 ==0 or sol1(N)
+
+cnt = 0
+for i in range(a, b+1):
+    if sol2(i):
+        cnt+=1
+        print(i)
+print(cnt)
+
+
+
+        
