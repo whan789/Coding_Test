053c18d74ff1aae43f5cf79e9c3a09786a2cf919
whan789 <102964433+whan789@users.noreply.github.com>
2026-05-03 17:18:00 +0900

    [Passed] 괄호 쌍 만들어주기 3 (45ms, 16MB)

---
+++ 260503/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,12 @@
+A = input()
+
+# Please write your code here.
+cnt = 0
+
+for i in range(len(A)):
+    if A[i] == '(':
+        for j in range(i+1,len(A)):
+            if A[j] == ')':
+                cnt+=1
+print(cnt)
+    
