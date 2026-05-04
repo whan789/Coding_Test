e6ba15da3941e5c98d79e7c8632de4b9365bfe49
whan789 <102964433+whan789@users.noreply.github.com>
2026-05-04 17:27:51 +0900

    [Passed] 마라톤 중간에 택시타기 2 (54ms, 16MB)

--- 260504/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 260504/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -7,9 +7,10 @@ y = [p[1] for p in points]
 # Please write your code here.
 INT_MAX = sys.maxsize
 min_dis = INT_MAX
-total_dis = 0
-L = []
+
 for i in range(1, len(points)-1):
+    L = []
+    total_dis = 0
     for j in range(len(points)):
         if j !=i:
             L.append((x[j],y[j]))
@@ -17,11 +18,8 @@ for i in range(1, len(points)-1):
     for k in range(len(L)-1):
         total_dis += abs(L[k+1][0]-L[k][0]) + abs(L[k+1][1]-L[k][1])
     
-    if min_dis > total_dis:
-        min_dis = total_dis
+    min_dis = min(min_dis, total_dis)
 
-    L = []
-    total_dis = 0
 print(min_dis)
 
 
