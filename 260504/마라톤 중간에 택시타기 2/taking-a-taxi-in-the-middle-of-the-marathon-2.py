4fcc2578e8c974b2172a97427cd78335c2616f29
whan789 <102964433+whan789@users.noreply.github.com>
2026-05-04 17:24:49 +0900

    [Passed] 마라톤 중간에 택시타기 2 (49ms, 16MB)

---
+++ 260504/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,28 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+
+# Please write your code here.
+min_dis = 10000000000000000
+total_dis = 0
+L = []
+for i in range(1, len(points)-1):
+    for j in range(len(points)):
+        if j !=i:
+            L.append((x[j],y[j]))
+    
+    for k in range(len(L)-1):
+        total_dis += abs(L[k+1][0]-L[k][0]) + abs(L[k+1][1]-L[k][1])
+    
+    if min_dis > total_dis:
+        min_dis = total_dis
+
+    L = []
+    total_dis = 0
+print(min_dis)
+
+
+
+
+    
