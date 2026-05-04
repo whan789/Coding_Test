7895b028e7141fab36d9040a57b751aa90f0f6e3
whan789 <102964433+whan789@users.noreply.github.com>
2026-05-04 11:49:04 +0900

    [Passed] 이상한 진수 2 (51ms, 16MB)

---
+++ 260504/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,21 @@
+a = input()
+A = list(a)
+max_num = 0
+total = 0
+# Please write your code here.
+for i in range(len(A)):
+    if A[i]== "1":
+        A[i] = "0"
+    else:
+        A[i] = "1"
+
+    for j in range(len(A)):
+        total = total + int(A[j]) * 2**(len(A)-j-1)
+    
+    if total > max_num:
+        max_num = total
+    A = list(a)
+    total = 0
+
+print(max_num)
+
